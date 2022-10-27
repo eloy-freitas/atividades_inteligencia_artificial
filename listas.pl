ddcb51dc59738ebc7761caef40593ddef8b10498
eloy-freitas <freitaseloy.1@gmail.com>
2022-10-26 21:56:27 -0300

    feat: exercicio 6, 7

--- listas.pl
+++ listas.pl
@@ -5,9 +5,31 @@ inverte([], L, L).
 % passo:
 inverte([A|B], Aux, Result) :- inverte(B, [A|Aux], Result).
 
-% exercicio 5
+% exercicio 6
 
 % base:
 membro([A|_], Elem) :- A = Elem, !.
 % passo:
 membro([_|R], Elem) :- membro(R, Elem). 
+
+% exercicio 7
+
+% base:
+itemDaLista([H|_], H, 0). 
+% passo
+itemDaLista([_|T], H, N) :-
+    Aux is N - 1,
+    itemDaLista(T, H, Aux).
+
+
+buscaItem([A|_], Elem, 1, 1) :-
+    Elem is A.
+
+% passo:
+buscaItem([_|R], Elem, N, Aux):-
+    Aux > 1,
+    Aux is N - 1,
+    buscaItem(R, Elem, N, Aux).
+
+
+
